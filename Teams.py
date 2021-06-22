5d5c0289fd2df38734edb5720985f3af412bc7a2
Shashank Ray <60728711+shashankray@users.noreply.github.com>
2021-06-22 19:34:37 +0530

    Add files via upload

---
+++ Teams.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+import os
+
+#os.system(os.environ['USERPROFILE'] + "\\AppData\\Local\\Microsoft\\Teams\\Update.exe --processStart \"Teams.exe\"")
+
+pyautogui.getWindowsWithTitle("Teams")[0].maximize()
+
+pyautogui.moveTo(238, 166, duration = 0.6)
+pyautogui.scroll(500)
+time.sleep(0.4)
+pyautogui.click( 238, 166)
+
+pyautogui.moveTo(544, 664, duration = 0.5)
+pyautogui.click( 544, 664)
+pyautogui.moveTo(824, 664)
+
+a = 0
+while(True):
+    a+=1
+    if a > 10:
+        pyautogui.hotkey('ctrl','a')
+        a = 0
+        if int(time.strftime("%H", time.localtime())) == 18:
+            os.system("shutdown /h")
+            exit()
+    pyautogui.typewrite("Hello ")
+    time.sleep(30)
